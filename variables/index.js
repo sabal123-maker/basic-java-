cb07c7bd1f6ef6d0dff1093f899744f93dcfb643
sabal dahal <sabaldahal721@gmail.com>
2024-10-23 18:58:17 +0545

    learn

---
+++ variables/index.js
@@ -0,0 +1,14 @@
+/* statusbg
+DocumentFragmewfa
+fawfwaaf*/
+
+
+console.log("information");
+var person="sabal";
+var club="brighton";
+console.log(person,club);
+const apple = "red apple";
+console.log("apple")
+let miko="fight";
+miko="fight";
+console.log("miko")
